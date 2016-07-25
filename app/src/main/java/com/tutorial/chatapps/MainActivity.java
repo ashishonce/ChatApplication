eb7d4d0ea9e5454c754b5affeefb7b97d4a9c356
Nazia Khan <nazia123khan@gmail.com>
2016-07-25 22:00:49 +0530

    Adding contact list

--- app/src/main/java/com/tutorial/chatapps/MainActivity.java
+++ app/src/main/java/com/tutorial/chatapps/MainActivity.java
@@ -5,13 +5,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.Date;
 
@@ -21,6 +25,9 @@ public class MainActivity extends Activity {
     private static final String TAG = "ChatActivity";
 
     private ChatArrayAdapter adp;
+    private  ChatArrayAdapter adp0;
+    private  ChatArrayAdapter adp1;
+    private  ChatArrayAdapter adp2;
     private ListView list;
     private EditText chatText;
     private Button send;
@@ -34,12 +41,47 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         Intent i = getIntent();
         setContentView(R.layout.main);
+        String[] items = new String[] { "Contact1", "Contact2", "Contact3" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+        Spinner dynamicSpinner = (Spinner) findViewById(R.id.dynamic_spinner);
+        dynamicSpinner.setAdapter(adapter);
+
+        dynamicSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+                if (position == 0) {
+                    adp = adp0;
+                    list.setAdapter(adp0);
+                }
+                else if(position ==1) {
+                    adp = adp1;
+                    list.setAdapter(adp1);
+                }
+                else if(position == 2) {
+                    adp = adp2;
+                    list.setAdapter(adp2);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+
 
         send = (Button) findViewById(R.id.btn);
 
         list = (ListView) findViewById(R.id.listview);
 
-        adp = new ChatArrayAdapter(getApplicationContext(), R.layout.chat);
+        adp0 = new ChatArrayAdapter(getApplicationContext(), R.layout.chat);
+        adp1 = new ChatArrayAdapter(getApplicationContext(), R.layout.chat);
+        adp2 = new ChatArrayAdapter(getApplicationContext(), R.layout.chat);
+        adp = adp0;
         list.setAdapter(adp);
 
         chatText = (EditText) findViewById(R.id.chat_text);
